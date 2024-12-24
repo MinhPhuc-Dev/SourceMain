e2421c2a4acb2247a84649181c9126bf1ca75dc4
MinhPhuc-Dev <nguyenhoangminhphuc2503@gmail.com>
2024-12-24 22:21:06 +0700

    Avade.lua

--- Avade.lua
+++ Avade.lua
@@ -1,21 +1,19 @@
 local HttpService = game:GetService("HttpService")
-
--- URL raw của file GitHub
 local url = "https://raw.githubusercontent.com/MinhPhuc-Dev/SourceMain/main/Avade.lua"
 
--- Sử dụng loadstring để tải và thực thi script
 local success, response = pcall(function()
     return HttpService:GetAsync(url)
 end)
 
 if success then
+    print("Script fetched successfully!")
     local scriptFunction, errorMessage = loadstring(response)
     if scriptFunction then
-        print("Script loaded successfully!")
-        scriptFunction() -- Chạy script
+        print("Executing script...")
+        scriptFunction()
     else
-        warn("Error executing script: " .. errorMessage)
+        warn("Error in script: ", errorMessage)
     end
 else
-    warn("Failed to fetch script: " .. response)
+    warn("Failed to fetch script: ", response)
 end
